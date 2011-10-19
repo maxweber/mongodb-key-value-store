7ca07d9ee33e939ac4d57a57bfc9a9e2b1d19240
Maximilian Weber <weber.maximilian@googlemail.com>
2011-10-19 10:47:39 +0200

    The project version has been corrected.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mongodb-key-value-store "1.0.0-SNAPSHOT"
+(defproject mongodb-key-value-store "0.1.0-SNAPSHOT"
   :description "Provides some functions to turn MongoDB into a plain key value store for Clojure data structures."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.7"]]
