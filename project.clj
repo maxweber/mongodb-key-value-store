c464e083bb0a75fed26a88f5347f37266a04a86e
Daniel Westheide <daniel@doo.net>
2012-01-30 15:18:34 +0100

    Updating congomongo dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject mongodb-key-value-store "0.1.0"
+(defproject org.clojars.doo/mongodb-key-value-store "0.1.1-SNAPSHOT"
   :description "Provides some functions to turn MongoDB into a plain key value store for Clojure data structures."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [congomongo "0.1.7"]]
+                 [org.clojars.doo/congomongo "0.1.8-SNAPSHOT"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]])
+                     [swank-clojure "1.4.0-SNAPSHOT"]
+                     [lein-clojars "0.7.0"]])
